863f7392232286896d90f9c16001e5b67a35ac40
TrevorEngland23 <trevor.england.523@gmail.com>
2024-12-04 15:22:56 +0000

    minor error checking, formatting

--- obj/Debug/net8.0/cloud-ai-challenge-project.AssemblyInfo.cs
+++ obj/Debug/net8.0/cloud-ai-challenge-project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cloud-ai-challenge-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5949dfe67286e700a7d506b5b65d5f1b9f748243")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2523cb728a010c73d8435fce3af7151d8753b125")]
 [assembly: System.Reflection.AssemblyProductAttribute("cloud-ai-challenge-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cloud-ai-challenge-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
